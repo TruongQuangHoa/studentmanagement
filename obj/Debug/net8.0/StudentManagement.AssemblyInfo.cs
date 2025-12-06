7d01215944f596cfc368bdaaf13144df058e4e59
Truong Quang Hoa <truongquanghoa19102004@gmail.com>
2025-12-06 23:23:38 +0700

    Resolve merge conflicts for sprint 8 & 9

--- obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1e63d286fa2be0c8d64f659c273c730d1172aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0675e3d0c1ef19f9b43d9cef04e6aa2159c5bf99")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
